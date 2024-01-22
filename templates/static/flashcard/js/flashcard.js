cc3d613bb29f3bf6c3d6a356cdf313ac84652a5a
Lucas Silva <131834120+LucasSilvaa0@users.noreply.github.com>
2024-01-21 21:39:02 -0300

    Add files via upload

---
+++ templates/static/flashcard/js/flashcard.js
@@ -0,0 +1,10 @@
+function flip_card(id) {
+    card = document.getElementById(id)
+    console.log(card.style.display)
+    if (card.style.display == "") {
+        card.style.display = "block"
+    } else {
+        card.style.display = "none"
+    }
+}
+
